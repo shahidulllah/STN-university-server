b855ce49c64bff1fc5ed7cbed0289d118f8759de
shahidullah177 <mdshahidsumon177@gmail.com>
2024-12-01 22:56:47 +0600

    Student controller created

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,5 +1,20 @@
+import { Request, Response } from "express"
+import { StudentServices } from "./student.service";
 
 
+const createStudent = async (req: Request, res: Response) => {
+    try {
+        const {student: StudentData} = req.body;
+        const result = await StudentServices.createStudentIntoDB(StudentData);
 
+        res.status(400).json({
+            success: true,
+            message: "Student is created successfully",
+            data: result,
+        })
+    }catch(err){
+        console.log(err);
+    }
+}
 
 export const studentControllers = {}
