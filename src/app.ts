865ba266502c077fef45ccdeabd89f73d20c4e30
shahidullah177 <mdshahidsumon177@gmail.com>
2024-12-02 14:11:25 +0600

    Route setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import { StudentRoutes } from './app/modules/student/student.route'
 
 const app: Application = express()
 
+//perser
+app.use(express.json())
+app.use(cors())
+
+//application routes
+app.use('api/v1/students', StudentRoutes)
+
+//checking
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -3,3 +3,5 @@ import express from 'express'
 const router = express.Router();
 
 router.post('/create-student', )
+
+export const StudentRoutes = router;
