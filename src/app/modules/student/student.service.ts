3d9ef06bb68e0682d56ff33c67a7e87ac95701f1
shahidullah177 <mdshahidsumon177@gmail.com>
2024-12-02 14:48:57 +0600

    Student service modify

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,13 +1,31 @@
-import { Student } from './student.interface'
-import { StudentModel } from './student.model'
+import { StudentModel } from "./student.interface";
+import { Student } from "./student.model";
 
-const createStudentIntoDB = async (student: Student) => {
-  const result = await StudentModel.create(student);
+
+const createStudentIntoDB = async (student: StudentModel) => {
+  const result = await Student.create(student);
 
   return result;
 }
 
+const getAllStudentsFromDB = async () => {
+  const result = await Student.find();
+  return result;
+};
+
+const getSingleStudentFromDB = async (id: string) => {
+  const result = await Student.aggregate([{ $match: { id } }]);
+  return result;
+};
+
+const deleteStudentFromDB = async (id: string) => {
+  const result = await Student.updateOne({ id }, { isDeleted: true });
+  return result;
+};
 
 export const StudentServices = {
     createStudentIntoDB,
+    getAllStudentsFromDB,
+    getSingleStudentFromDB,
+    deleteStudentFromDB,
 }
