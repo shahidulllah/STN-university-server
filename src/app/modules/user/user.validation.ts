29c333332b738d50bed1162135d23e3e5ff7e310
shahidullah177 <mdshahidsumon177@gmail.com>
2024-12-04 20:07:20 +0600

    user validation modify

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,12 +1,16 @@
 import { z } from 'zod';
 
 const userValidationSchema = z.object({
+  id: z.string(),
   pasword: z
     .string({
       invalid_type_error: 'Password must be string',
     })
-    .max(20, { message: 'Password can not be more than 20 characters' })
-    .optional(),
+    .max(20, { message: 'Password can not be more than 20 characters' }),
+    // .optional(),
+    needsPasswordChange: z.boolean().optional().default(true),
+    status: z.enum(['in-progress', 'blocked']).default('in-progress'),
+    isDeleted: z.boolean().optional().default(false),
 });
 
 export const UserValidation = {
