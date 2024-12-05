fa5e8adaa087187a086b8cc0ec7bffb25eff9518
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-05 12:54:02 +0600

    academic semester route

---
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidations } from './academicSemester.validation';
+import { AcademicSemesterControllers } from './academicSemester.controller';
+
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-semester',
+  validateRequest(
+    AcademicSemesterValidations.createAcdemicSemesterValidationSchema,
+  ),
+  AcademicSemesterControllers.createAcademicSemester,
+);
+
+router.get(
+  '/:semesterId',
+  AcademicSemesterControllers.getSingleAcademicSemester,
+);
+
+router.patch(
+  '/:semesterId',
+  validateRequest(
+    AcademicSemesterValidations.updateAcademicSemesterValidationSchema,
+  ),
+  AcademicSemesterControllers.updateAcademicSemester,
+);
+
+router.get('/', AcademicSemesterControllers.getAllAcademicSemesters);
+
+export const AcademicSemesterRoutes = router;
