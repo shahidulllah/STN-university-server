d7e4ebf5f4561da30e580eb2f1d11970f9ddccb6
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-06 12:33:44 +0600

    modify

--- src/app/modules/academicFaculty/academicFaculty.service.ts
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -1,20 +1,21 @@
-import { TAcademicFaculty } from './academicFaculty.interface'
-import { AcademicFaculty } from './academicFaculty.model'
+import { TAcademicFaculty } from "./academicFaculty.interface";
+import { AcademicFaculty } from "./academicFaculty.model";
+
 
 const createAcademicFacultyIntoDB = async (payload: TAcademicFaculty) => {
-  const result = await AcademicFaculty.create(payload)
-  return result
-}
+  const result = await AcademicFaculty.create(payload);
+  return result;
+};
 
 const getAllAcademicFacultiesFromDB = async () => {
-  const result = await AcademicFaculty.find()
-  return result
-}
+  const result = await AcademicFaculty.find();
+  return result;
+};
 
 const getSingleAcademicFacultyFromDB = async (id: string) => {
-  const result = await AcademicFaculty.findById(id)
-  return result
-}
+  const result = await AcademicFaculty.findById(id);
+  return result;
+};
 
 const updateAcademicFacultyIntoDB = async (
   id: string,
@@ -22,13 +23,13 @@ const updateAcademicFacultyIntoDB = async (
 ) => {
   const result = await AcademicFaculty.findOneAndUpdate({ _id: id }, payload, {
     new: true,
-  })
-  return result
-}
+  });
+  return result;
+};
 
 export const AcademicFacultyServices = {
   createAcademicFacultyIntoDB,
   getAllAcademicFacultiesFromDB,
   getSingleAcademicFacultyFromDB,
   updateAcademicFacultyIntoDB,
-}
+};
