2183ee661f48dd00c1d980fecdf6fa9b2d1a0a71
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-06 11:41:46 +0600

    academic department model

---
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -0,0 +1,56 @@
+import { model, Schema } from "mongoose";
+import { TAcademicDepartment } from "./academicDepartment.route";
+import AppError from "../../errors/AppError";
+import { StatusCodes } from "http-status-codes";
+
+
+const academicDepartmentSchema = new Schema<TAcademicDepartment>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExist) {
+    throw new AppError(
+      StatusCodes.NOT_FOUND,
+      'This department is already exist!',
+    );
+  }
+
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicDepartment.findOne(query);
+
+  if (!isDepartmentExist) {
+    throw new AppError(
+        StatusCodes.NOT_FOUND,
+      'This department does not exist! ',
+    );
+  }
+
+  next();
+});
+
+export const AcademicDepartment = model<TAcademicDepartment>(
+  'AcademicDepartment',
+  academicDepartmentSchema,
+);
