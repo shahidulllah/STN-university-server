d8f575f3d6608af9250eb1e40e49ccc6ea5ad439
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-14 00:06:42 +0600

    worikng on jwt access token

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -9,4 +9,6 @@ export default {
   bcrypt_salt_rounds: process.env.BYCRYPT_SALT_ROUNDS,
   default_password: process.env.DEFAULT_PASS,
   NODE_ENV: process.env.NODE_ENV,
+  jwt_access_secret: process.env.JWT_ACCESS_SECTET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN 
 }
