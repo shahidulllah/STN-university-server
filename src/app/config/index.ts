0f17bb5a4261281b634ab0dc422450617d196610
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-14 00:11:33 +0600

    worikign on env

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -9,6 +9,9 @@ export default {
   bcrypt_salt_rounds: process.env.BYCRYPT_SALT_ROUNDS,
   default_password: process.env.DEFAULT_PASS,
   NODE_ENV: process.env.NODE_ENV,
-  jwt_access_secret: process.env.JWT_ACCESS_SECTET,
-  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN 
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  
 }
