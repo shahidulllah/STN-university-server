13f3967490cbef4b1dc20b8f4f0cc37fb2c4f373
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-13 23:53:58 +0600

    working on user service

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,7 +1,13 @@
-import { User } from "../user/user.model";
-import { TLoginUser } from "./auth.interface";
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
+import { TLoginUser } from './auth.interface';
 
-const loginUser = async (payload: TLoginUser) =>{
-    //checking if the user is already exist
-    const user = await User.isUserExi
-}
+const loginUser = async (payload: TLoginUser) => {
+  //checking if the user is already exist
+  const user = await User.isUserExistsByCustomId(payload.id);
+
+  if (!user) {
+    throw new AppError(StatusCodes.NOT_FOUND, 'This user is not found');
+  }
+};
