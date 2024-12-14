f9a3866e7e19ea00bace69393639e3873f90d8d5
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-14 12:29:57 +0600

    faculty routes

--- src/app/modules/Faculty/faculty.route.ts
+++ src/app/modules/Faculty/faculty.route.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
+import { USER_ROLE } from '../user/user.constant';
 import { FacultyControllers } from './faculty.controller';
 import { updateFacultyValidationSchema } from './faculty.validation';
 
 const router = express.Router();
-
-router.post('/create-faculties', FacultyControllers.createFaculty);
 
 router.get('/:id', FacultyControllers.getSingleFaculty);
 
@@ -17,6 +17,10 @@ router.patch(
 
 router.delete('/:id', FacultyControllers.deleteFaculty);
 
-router.get('/', FacultyControllers.getAllFaculties);
+router.get(
+  '/',
+  auth(USER_ROLE.admin, USER_ROLE.faculty),
+  FacultyControllers.getAllFaculties,
+);
 
 export const FacultyRoutes = router;
