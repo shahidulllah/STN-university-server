f72e4f48c15baaaa2a7e30e73a277eb0e4ca314c
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-23 20:51:29 +0600

    woriking env

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv'
-import path from 'path'
+import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({ path: path.join((process.cwd(), '.env')) })
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
 export default {
   port: process.env.PORT,
@@ -13,5 +13,4 @@ export default {
   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
   jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
-  
-}
+};
