02097e64d36435e887c518f101899e85d6c0c72b
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-27 18:42:45 +0600

    upload route added in create student

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { createAdminValidationSchema } from '../Admin/admin.validation';
@@ -7,12 +7,19 @@ import { createStudentValidationSchema } from './../student/student.validation';
 import { USER_ROLE } from './user.constant';
 import { UserControllers } from './user.controller';
 import { UserValidation } from './user.validation';
+import { upload } from '../../utils/sendImageToCloudinary';
 
 const router = express.Router();
 
 router.post(
   '/create-student',
-  auth(USER_ROLE.admin),
+  // auth(USER_ROLE.admin),
+  upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body);
+    req.body = JSON.parse(req.body.data);
+    next();
+  },
   validateRequest(createStudentValidationSchema),
   UserControllers.createStudent,
 );
