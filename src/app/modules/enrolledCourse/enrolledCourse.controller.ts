6fa47ad45a4b27464aa50cbe1d843dae4dd0c152
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-28 15:42:07 +0600

    enrolled course controller

---
+++ src/app/modules/enrolledCourse/enrolledCourse.controller.ts
@@ -0,0 +1,39 @@
+import { StatusCodes } from 'http-status-codes';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { EnrolledCourseServices } from './enrolledCourse.service';
+
+const createEnrolledCourse = catchAsync(async (req, res) => {
+  const userId = req.user.userId;
+  const result = await EnrolledCourseServices.createEnrolledCourseIntoDB(
+    userId,
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Student is enrolled succesfully',
+    data: result,
+  });
+});
+
+const updateEnrolledCourseMarks = catchAsync(async (req, res) => {
+  const facultyId = req.user.userId;
+  const result = await EnrolledCourseServices.updateEnrolledCourseMarksIntoDB(
+    facultyId,
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Marks is updated succesfully',
+    data: result,
+  });
+});
+
+export const EnrolledCourseControllers = {
+  createEnrolledCourse,
+  updateEnrolledCourseMarks,
+};
