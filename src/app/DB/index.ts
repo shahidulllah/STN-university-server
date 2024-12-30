f68759cdb58a84c6e799e1f9e2e5050ab63bd5ec
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-30 10:38:22 +0600

    DB is created

--- src/app/DB/index.ts
+++ src/app/DB/index.ts
@@ -1,5 +1,6 @@
 import config from '../config';
 import { USER_ROLE } from '../modules/user/user.constant';
+import { User } from '../modules/user/user.model';
 
 const superUser = {
   id: '0001',
@@ -10,3 +11,13 @@ const superUser = {
   status: 'in-progress',
   isDeleted: false,
 };
+
+const seedSuperAdmin = async () => {
+  const isSuperAdminExists = await User.findOne({ role: USER_ROLE.superAdmin });
+
+  if (!isSuperAdminExists) {
+    await User.create(superUser);
+  }
+};
+
+export default seedSuperAdmin;
